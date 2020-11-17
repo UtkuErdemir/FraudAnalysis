18237b6d6f8b2d2b17a24dc234b1b357ac8f8dcf
Utku Erdemir <56741781+UtkuErdemir@users.noreply.github.com>
2020-11-17 22:45:54 +0300

    Density Graphic added
    Density graphic and some code optimizations added.

--- main.py
+++ main.py
@@ -1,38 +1,45 @@
-
+import seaborn as sns
 import numpy as np
 
 from consoleOperations import create_line_break
 from fileOperations import get_csv
 import matplotlib.pyplot as plt
+normalTransactions = get_csv().loc[get_csv()['Class'] == 0]
+fraudTransactions = get_csv().loc[get_csv()['Class'] == 1]
 print('Veri setindeki sütunlar:', get_csv().shape[1], 'adet sütun bulunmaktadır. Bunlar isimleri ve veri tipleri ile beraber aşağıda listelenmiştir.')
 create_line_break()
 print(get_csv().info())
 create_line_break()
 print('Veri setindeki satır sayısı:', get_csv().shape[0])
 create_line_break()
-print('Veri setindeki dolandırıcılık işlemi olan veri sayısı:',len(get_csv().loc[get_csv().Class==1]))
-print('Veri setindeki dolandırıcılık işlemi olanların yüzdesi: %',100*len(get_csv().loc[get_csv().Class==1])/get_csv().shape[0])
+print('Veri setindeki dolandırıcılık işlemi olan veri sayısı:',len(fraudTransactions))
+print('Veri setindeki dolandırıcılık işlemi olanların yüzdesi: %',100*len(fraudTransactions)/get_csv().shape[0])
 create_line_break()
-print('Veri setindeki normal işlemlerin veri sayısı:',len(get_csv().loc[get_csv().Class==0]))
-print('Veri setindeki normal işlemlerin yüzdesi: %',100*len(get_csv().loc[get_csv().Class==0])/get_csv().shape[0])
+print('Veri setindeki normal işlemlerin veri sayısı:',len(normalTransactions))
+print('Veri setindeki normal işlemlerin yüzdesi: %',100*len(normalTransactions)/get_csv().shape[0])
 create_line_break()
 print('Bu sonuçlardan veri setinin düzensiz şekilde dağıldığını söyleyebiliriz.')
 create_line_break()
 print('Veri setindeki ilk on veri:\n', get_csv().head(10))
 create_line_break()
-print('Veri setindeki normal işlemlerin istatistiksel incelenmesi:\n', get_csv().loc[get_csv().Class==0].describe())
+print('Veri setindeki normal işlemlerin istatistiksel incelenmesi:\n',normalTransactions.describe())
 create_line_break()
 classTypes = [0, 1]
 classNames = ["Normal", "Dolandırıclık"]
-classResults = [len(get_csv().loc[get_csv().Class==0]), len(get_csv().loc[get_csv().Class == 1])]
+classResults = [len(normalTransactions), len(fraudTransactions)]
 plt.bar(classTypes, classResults, color='purple')
 plt.xlabel("Yapılan İşlemler")
 plt.ylabel("İşlem Sayısı")
 plt.title("Yapılan İşlemlerin Dağılımı")
 plt.xticks(classTypes, [name + " (" + str(classResults[i]) + ")" for i, name in enumerate(classNames)])
 plt.show()
-plt.scatter(get_csv().loc[get_csv()['Class'] == 0]['V1'], get_csv().loc[get_csv()['Class'] == 0]['V2'], label="Class #0", alpha=0.5, linewidth=0.15)
-plt.scatter(get_csv().loc[get_csv()['Class'] == 1]['V1'], get_csv().loc[get_csv()['Class'] == 1]['V2'], label="Class #1", alpha=0.5, linewidth=0.15,c='r')
+plt.scatter(normalTransactions['V1'], normalTransactions['V2'], label="Normal İşlemler", alpha=1, linewidth=0.10,c='r')
+plt.scatter(fraudTransactions['V1'], fraudTransactions['V2'], label="Dolandırıclık İşlemleri", alpha=0.3, linewidth=0.50,c='g')
+plt.show()
+plt.title('İşlemlerin Zaman Yoğunluğu Grafiği')
+plt.figure(figsize = (10,3))
+sns.distplot(normalTransactions['Time'],kde=True,bins=240)
+sns.distplot(fraudTransactions['Time'],kde=True,bins=240)
 plt.show()
 create_line_break()
 print('Veri setindeki eksik verilerin yüzdesi:\n'+str(get_csv().isnull().sum() / get_csv().count()))
